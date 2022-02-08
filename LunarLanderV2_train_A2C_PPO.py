69347cfa086d2fbb7ae628951297d559353dd37b
Brian Lee <b.lee@utexas.edu>
2022-02-08 15:29:29 +0900

    completed training code for A2C and PPO, LunarLander-v2

---
+++ LunarLanderV2_train_A2C_PPO.py
@@ -0,0 +1,56 @@
+# %%
+import os
+import gym
+from datetime import datetime
+from stable_baselines3 import A2C, PPO
+
+# %% Input variables
+MODELS_DIR = './models/PPO'
+LOG_DIR = './logs/'
+SAVE_EVERY = 10000 # Save once every N iterations
+TOTAL_TIMESTEPS = SAVE_EVERY * 30
+MODEL = 'A2C'
+
+# %% Get timestamp string
+now = datetime.now()
+current_time_code = now.strftime("%Y%m%d_%H%M%S")
+print('Current time code:', current_time_code)
+
+# %% Create directories if nonexistent
+if not os.path.exists(MODELS_DIR):
+    os.makedirs(MODELS_DIR)
+if not os.path.exists(os.path.join(MODELS_DIR, current_time_code)):
+    os.makedirs(os.path.join(MODELS_DIR, current_time_code))
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+# %% Train the model
+env = gym.make('LunarLander-v2')
+env.reset()
+
+if MODEL == 'A2C':
+    model = A2C('MlpPolicy', env, verbose=1, tensorboard_log=LOG_DIR + current_time_code)
+elif MODEL == 'PPO':
+    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=LOG_DIR + current_time_code)
+
+for i in range(TOTAL_TIMESTEPS//SAVE_EVERY):
+    model.learn(total_timesteps=SAVE_EVERY, reset_num_timesteps=False, tb_log_name=MODEL)
+    model.save(f'{MODELS_DIR}/{MODEL}_{SAVE_EVERY*i}')
+
+print('Sample Action:', env.action_space.sample())
+print('Observation Space Shape: ', env.observation_space.shape)
+print('Sample Observation', env.observation_space.sample())
+
+# %% Try out the final model
+episodes = 10
+for ep in range(episodes):
+    obs = env.reset()
+    done = False
+
+    while not done:
+        env.render()
+        # action = env.action_space.sample()
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+
+env.close()
